ae607ee82477fda9f0ae0af52881f87959f0bd4d
Elian-dot <mejiagutierrezeliancamilo@outlook.com>
2024-12-22 20:23:15 -0500

    Crear Header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,24 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#167DB7',
+				accent: "#FE5230",
+				"ij-black": "#212121",
+				"ij-red": "#FF421C",
+				"ij-green": "#00A550",
+				"ij-blue": "#E8F2F8",
+				"ij-yellow": "#EFA500",
+				dark: '',
+			},
+			backgroundImage: {
+				"hero-pattern": "url(/hero-pattern.webp)",
+			},
+			borderRadius:{
+				'4xl': '3rem'
+			}
+		},
 	},
 	plugins: [],
 }
